a41f8e5e1369cef7903244d4d48fc5d471642a9f
zmj <2770322891@qq.com>
2022-12-10 01:11:12 +0800

    账户微服务环境搭建

---
+++ dcloud-account/src/main/java/net/xdclass/account/AccountApplication.java
@@ -0,0 +1,17 @@
+package net.xdclass.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableFeignClients
+@EnableDiscoveryClient
+public class AccountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+}
